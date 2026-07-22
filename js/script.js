80c0af8849ac331c61d90715565e9e87eb6d1184
miki <love.sana.0327@gmail.com>
2026-07-23 00:07:59 +0900

    フォームバリデーションの追加からナビの調整、カフェサイトの統一

--- js/script.js
+++ js/script.js
@@ -3,4 +3,15 @@ const navMenu = document.querySelector('.nav-menu');
 
 menuToggle.addEventListener('click', () => {
   navMenu.classList.toggle('is-open');
+  menuToggle.classList.toggle('is-active');
+});
+
+// nav-menuの中の全リンクを取得
+const navLinks = document.querySelectorAll('.nav-menu a');
+
+// リンクが1個ずつクリックされたら、メニューを閉じる
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('is-open');
+  });
 });
